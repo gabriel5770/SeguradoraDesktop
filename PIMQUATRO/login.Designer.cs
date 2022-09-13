b556e4f17bdc97ee53651608b81a7653d335167f
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-09-12 21:53:57 -0300

    login.cs: Inserção da lógica de login;

--- PIMQUATRO/login.Designer.cs
+++ PIMQUATRO/login.Designer.cs
@@ -35,7 +35,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -45,10 +45,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(14, 16);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(109, 127);
+            this.pictureBox2.Size = new System.Drawing.Size(95, 95);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
@@ -56,10 +55,9 @@
             // 
             this.Voltar.BackColor = System.Drawing.Color.CornflowerBlue;
             this.Voltar.ForeColor = System.Drawing.Color.Snow;
-            this.Voltar.Location = new System.Drawing.Point(854, 16);
-            this.Voltar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Voltar.Location = new System.Drawing.Point(747, 12);
             this.Voltar.Name = "Voltar";
-            this.Voltar.Size = new System.Drawing.Size(86, 31);
+            this.Voltar.Size = new System.Drawing.Size(75, 23);
             this.Voltar.TabIndex = 3;
             this.Voltar.Text = "Voltar";
             this.Voltar.UseVisualStyleBackColor = false;
@@ -69,10 +67,9 @@
             // 
             this.Ajuda.BackColor = System.Drawing.Color.CornflowerBlue;
             this.Ajuda.ForeColor = System.Drawing.Color.Snow;
-            this.Ajuda.Location = new System.Drawing.Point(854, 568);
-            this.Ajuda.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Ajuda.Location = new System.Drawing.Point(747, 426);
             this.Ajuda.Name = "Ajuda";
-            this.Ajuda.Size = new System.Drawing.Size(86, 31);
+            this.Ajuda.Size = new System.Drawing.Size(75, 23);
             this.Ajuda.TabIndex = 4;
             this.Ajuda.Text = "Ajuda";
             this.Ajuda.UseVisualStyleBackColor = false;
@@ -80,20 +77,18 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(392, 248);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(343, 186);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 40);
+            this.pictureBox1.Size = new System.Drawing.Size(31, 30);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(392, 303);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(343, 227);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(35, 39);
+            this.pictureBox3.Size = new System.Drawing.Size(31, 29);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
@@ -102,33 +97,30 @@
             // 
             this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtEmail.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.txtEmail.Location = new System.Drawing.Point(434, 267);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtEmail.Location = new System.Drawing.Point(380, 200);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(166, 20);
+            this.txtEmail.Size = new System.Drawing.Size(145, 16);
             this.txtEmail.TabIndex = 7;
             this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // txtSenha
             // 
-            this.textBox2.AccessibleName = "txtEmail";
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.textBox2.Location = new System.Drawing.Point(434, 320);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(166, 20);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtSenha.AccessibleName = "txtEmail";
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtSenha.Location = new System.Drawing.Point(380, 240);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(145, 16);
+            this.txtSenha.TabIndex = 8;
+            this.txtSenha.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.CornflowerBlue;
             this.button4.ForeColor = System.Drawing.Color.Snow;
-            this.button4.Location = new System.Drawing.Point(461, 349);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Location = new System.Drawing.Point(403, 289);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(86, 31);
+            this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 10;
             this.button4.Text = "Entrar";
             this.button4.UseVisualStyleBackColor = false;
@@ -136,18 +128,17 @@
             // 
             // PageLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 615);
+            this.ClientSize = new System.Drawing.Size(834, 461);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtSenha);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Ajuda);
             this.Controls.Add(this.Voltar);
             this.Controls.Add(this.pictureBox2);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "PageLogin";
             this.Text = "login";
             this.Load += new System.EventHandler(this.PageLogin_Load);
@@ -167,7 +158,7 @@
         private PictureBox pictureBox1;
         private PictureBox pictureBox3;
         private TextBox txtEmail;
-        private TextBox textBox2;
+        private TextBox txtSenha;
         private Button button4;
     }
 }
--- PIMQUATRO/login.cs
+++ PIMQUATRO/login.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,9 @@ namespace PIMQUATRO
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
-         
+
         private void Voltar_Click_1(object sender, EventArgs e)
         {
             this.Hide();
@@ -30,7 +32,7 @@ namespace PIMQUATRO
 
         private void PageLogin_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -40,8 +42,34 @@ namespace PIMQUATRO
 
         private void button4_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            new menu().ShowDialog();
+            string Email = txtEmail.Text;
+            string Senha = txtSenha.Text;
+
+
+            var queryString = $"SELECT TOP(1) * FROM tbLogin WITH(NOLOCK) WHERE Email = '{Email}' AND SENHA ='{Senha}'";
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ToString()))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                        this.Hide();
+                        new menu().ShowDialog();
+                        }                       
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("erro: " + ex);
+                }
+            }
         }
     }
+
 }
