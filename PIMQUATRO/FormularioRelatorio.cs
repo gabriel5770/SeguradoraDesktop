c02dbefde83b27c14af5da57b1b4eeb53555abe8
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-11-14 20:35:39 -0300

    Retorno dos dados da apólice no reader

--- PIMQUATRO/FormularioRelatorio.cs
+++ PIMQUATRO/FormularioRelatorio.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,10 +21,55 @@ namespace PIMQUATRO
 
         private void btnGerarApolice_Click(object sender, EventArgs e)
         {
-
+            if (RetornaPdfApolice())
+            {
+                MessageBox.Show("Apólice criado com sucesso");
+            };
         }
 
+           
+        private bool RetornaPdfApolice()
+        {
+            string Nome = "";
+            string Cpf = "";
+            string DataVigenciaInicial = "";
+            string DataVigenciaFinal = "";
+            string NumeroContrato = "";
 
+            bool rtnValido = false;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ToString()))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(
+                                    $"SELECT tbclientes.nome, tbclientes.cpf, tbapolice.dataVigenciaInicial, tbapolice.dataVigenciaFinal, tbapolice.NumeroContrato  FROM tbapolice inner join tbclientes ON tbapolice.id = tbclientes.idApolice where tbclientes.cpf = {txtCpfCliente.Text}", connection))
+
+                    try
+                    {
+                         SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Nome = reader.GetString(0);
+                            Cpf = reader.GetString(1);
+                            DataVigenciaInicial = Convert.ToString(reader.GetDateTime(2));
+                            DataVigenciaFinal = Convert.ToString(reader.GetDateTime(3));
+                            NumeroContrato = Convert.ToString(reader.GetInt64(4));
+
+
+                            rtnValido = true;
+                            return rtnValido;
+                        }
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Erro ao gerar apólice");
+                        MessageBox.Show("Erro encontrado: " + ex);
+
+                    }
+                return rtnValido;
+            }
+        }
 
     }
 }
