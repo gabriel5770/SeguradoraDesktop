e3559e36aeea0ee72eabe4b6fb5ba91bb4582ba3
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-11-15 23:34:14 -0300

    Inserção da classe contrato

---
+++ PIMQUATRO/Modelo/Contrato.cs
@@ -0,0 +1,22 @@
+﻿using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMQUATRO.Modelo
+{
+    public class Contrato
+    {
+        private int _numeroContrato { get; set; }
+        private string _nomeCliente { get; set; }
+        private string _NomeBeneficiario { get; set; }
+        private string _tipoPlano { get; set; }
+        private string _nomeSeguradora { get; set; }
+        private string _cpfCliente { get;set; }
+        private string _cnpjEmpresa { get; set; }
+
+
+    }
+}
