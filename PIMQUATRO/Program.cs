f20cff32abd80f559673a1ada506325caf474570
Gabriel Souza <gabrielsouza6835@gmail.com>
2022-12-04 12:47:52 -0300

    inicio

--- PIMQUATRO/Program.cs
+++ PIMQUATRO/Program.cs
@@ -11,7 +11,7 @@ namespace PIMQUATRO
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FormularioMenu());
+            Application.Run(new FormularioInicio());
         }
     }
 }
